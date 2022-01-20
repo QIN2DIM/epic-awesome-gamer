e87359ba641b32cf1a33f84c0d0b814bfce269cc
beiyuouo <beiyu233@gmail.com>
2022-01-20 20:48:28 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -9,17 +9,14 @@ from typing import Dict, Union
 import pytz
 
 from services.utils import ToolBox
-
 """
 ================================================ ʕ•ﻌ•ʔ ================================================
                             (·▽·)欢迎嫖友入座，请跟随提示合理配置项目启动参数
 ================================================ ʕ•ﻌ•ʔ ================================================
 [√]核心配置 [※]边缘参数
 """
-config_ = ToolBox.check_sample_yaml(
-    path_output=join(dirname(__file__), "config.yaml"),
-    path_sample=join(dirname(__file__), "config-sample.yaml")
-)
+config_ = ToolBox.check_sample_yaml(path_output=join(dirname(__file__), "config.yaml"),
+                                    path_sample=join(dirname(__file__), "config-sample.yaml"))
 
 # --------------------------------
 # [√]Account
@@ -30,8 +27,9 @@ USER_PASSWORD = config_["PASSWORD"]
 # --------------------------------
 # [√]Scheduled task configuration
 # --------------------------------
-SCHEDULER_SETTINGS: Dict[str, Union[int, bool]] = config_["scheduler"]
-
+SCHEDULER_SETTINGS: Dict[str,
+                         Union[int,
+                               bool]] = config_["scheduler"] if "scheduler" in config_ else {}
 """
 ================================================== ʕ•ﻌ•ʔ ==================================================
                                   若您并非项目开发者 请勿修改以下变量的默认参数
