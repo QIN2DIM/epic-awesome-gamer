0369915131f3f41130f795b818db95ef384a7b4c
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2022-02-11 06:11:15 +0800

    fix(TypeError) : expected str, bytes or os.PathLike object, not NoneType (https://github.com/QIN2DIM/epic-awesome-gamer/issues/23)
    提示玩家手动安装缺失的 `google-chrome`

--- src/apis/scaffold/install.py
+++ src/apis/scaffold/install.py
@@ -3,50 +3,41 @@
 # Author     : QIN2DIM
 # Github     : https://github.com/QIN2DIM
 # Description:
+import sys
+import webbrowser
+
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import get_browser_version_from_os
 
 from services.settings import DIR_MODEL, logger
-from services.utils import YOLO, CoroutineSpeedup, ToolBox
+from services.utils import YOLO, ToolBox
 from services.utils import get_challenge_ctx
 
 
 def _download_model():
-    """
-    下载 YOLOv4 目标检测模型
-
-    :return:
-    """
+    """下载 YOLOv4 目标检测模型"""
     logger.debug("下载 YOLOv4 目标检测模型...")
     YOLO(dir_model=DIR_MODEL).download_model()
 
 
 def _download_driver():
-    """
-    下载浏览器驱动。
-
-    :return:
-    """
+    """下载浏览器驱动"""
     logger.debug("适配 ChromeDriver...")
-    ChromeDriverManager(version="latest").install()
 
+    browser_version = get_browser_version_from_os("google-chrome")
+    if browser_version != "UNKNOWN":
+        return ChromeDriverManager(version="latest").install()
 
-class PerformanceReleaser(CoroutineSpeedup):
-    """协程助推器 并发执行片段代码"""
-
-    def __init__(self, docker, power=None):
-        super().__init__(docker=docker, power=power)
-
-    @logger.catch()
-    def control_driver(self, task, *args, **kwargs):
-        task()
+    logger.critical("当前环境变量缺少 `google-chrome`，请为你的设备手动安装 Chrome 浏览器。")
+    logger.info("Ubuntu: https://linuxize.com/post/how-to-install-google-chrome-web-browser-on-ubuntu-20-04/")
+    logger.info("CentOS 7/8: https://linuxize.com/post/how-to-install-google-chrome-web-browser-on-centos-7/")
+    if "linux" not in sys.platform:
+        webbrowser.open("https://www.google.com/chrome/")
+    logger.info("安装完毕后重新执行 `install` 脚手架指令。")
 
 
 def run():
-    """
-    下载项目运行所需的各项依赖。
-
-    :return:
-    """
+    """下载项目运行所需的各项依赖"""
     logger.debug(
         ToolBox.runtime_report(
             motive="BUILD",
@@ -55,7 +46,8 @@ def run():
         )
     )
 
-    PerformanceReleaser(docker=[_download_driver, _download_model], power=3).speedup()
+    _download_driver()
+    _download_model()
 
     logger.success(
         ToolBox.runtime_report(
