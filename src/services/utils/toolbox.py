090d6273d7c610262fd6848924bf43c4bce2a9ea
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2022-09-26 20:58:03 +0800

    Update toolbox.py

--- src/services/utils/toolbox.py
+++ src/services/utils/toolbox.py
@@ -225,10 +225,9 @@ def get_challenge_ctx(silence: typing.Optional[bool] = None) -> Challenger:
     """挑战者驱动 用于处理人机挑战"""
     driver_wrapper = DriverWrapper(silence=silence)
     options = driver_wrapper.options
-
-    if "linux" in sys.platform and silence:
-        logger.critical("CMD: Xvfb -ac >/dev/null 2>&1 & python3 main.py claim")
-        raise RuntimeError("Please use `xvfb` to empower the headful Chrome.")
+    if "linux" in sys.platform:
+        logger.warning("CMD: Xvfb -ac >/dev/null 2>&1 & python3 main.py claim")
+        assert silence, "Please use `xvfb` to empower the headful Chrome."
     # Create challenger
     logging.debug(ToolBox.runtime_report("__Context__", "ACTIVATE", "🎮 激活挑战者上下文"))
     return Challenger(options=options, driver_executable_path=driver_wrapper.path)
