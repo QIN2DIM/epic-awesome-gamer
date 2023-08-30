d6bbc610b85f3e507e47d99fe9682154a0a47f9c
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-08-30 18:46:11 +0800

    Update epic_games.py

--- src/services/agents/epic_games.py
+++ src/services/agents/epic_games.py
@@ -193,7 +193,7 @@ class EpicGames:
 
         fall_in_challenge = False
 
-        for _ in range(8):
+        for _ in range(15):
             if not fall_in_challenge:
                 with suppress(TimeoutError):
                     page.wait_for_url(URL_CART_SUCCESS, timeout=10000)
